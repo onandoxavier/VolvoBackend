6ccb9f6c26596be2eb2bb6fe8e41d4fda728bdae
Fernando Xavier <fer-xavier@hotmail.com>
2024-10-30 03:17:17 -0300

    altera cors para qualquer origen dado o cenario de teste e validacao

--- Volvo.API/Program.cs
+++ Volvo.API/Program.cs
@@ -11,9 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins(
-                "http://localhost:3000", 
-                "http://localhost:5173")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
